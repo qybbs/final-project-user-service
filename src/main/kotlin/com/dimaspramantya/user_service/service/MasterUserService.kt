cc88f08860383e40d579785231ad2ccd54fa7c09
galihnugraha <galihnugraha58@gmail.com>
2025-06-12 18:18:58 +0700

    [add] jwt

--- src/main/kotlin/com/dimaspramantya/user_service/service/MasterUserService.kt
+++ src/main/kotlin/com/dimaspramantya/user_service/service/MasterUserService.kt
@@ -1,7 +1,12 @@
 package com.dimaspramantya.user_service.service
 
+import com.dimaspramantya.user_service.domain.dto.request.ReqLoginDto
+import com.dimaspramantya.user_service.domain.dto.request.ReqRegisterDto
 import com.dimaspramantya.user_service.domain.dto.response.ResGetUsersDto
+import com.dimaspramantya.user_service.domain.dto.response.ResLoginDto
 
 interface MasterUserService {
     fun findAllActiveUsers(): List<ResGetUsersDto>
+    fun register(req: ReqRegisterDto): ResGetUsersDto
+    fun login(req: ReqLoginDto): ResLoginDto
 }
